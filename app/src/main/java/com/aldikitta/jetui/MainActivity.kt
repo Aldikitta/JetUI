b36f0a8d88cce6fc7455d8106aa344a2308c89e7
Aldikitta <andi.kitta77@gmail.com>
2022-06-15 13:49:55 +0800

    add drawer

--- app/src/main/java/com/aldikitta/jetui/MainActivity.kt
+++ app/src/main/java/com/aldikitta/jetui/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -220,144 +221,11 @@ fun Drawer() {
         }
     }
 }
-//
-//@OptIn(ExperimentalMaterial3Api::class)
-//@Composable
-//fun Navigation() {
-//    val navController = rememberNavController()
-//    val items = listOf(
-//        NavigationItem.Home,
-//        NavigationItem.Settings,
-//        NavigationItem.Notifications,
-//        NavigationItem.Account
-//    )
-//    val snackbarHostState = remember {
-//        SnackbarHostState()
-//    }
-//    val scope = rememberCoroutineScope()
-//    var expanded by remember { mutableStateOf(false) }
-//    Scaffold(
-//        snackbarHost = { SnackbarHost(snackbarHostState) },
-//        topBar = {
-//            SmallTopAppBar(
-//                title = { Text(text = "Netflix") },
-//                navigationIcon = {
-//                    IconButton(onClick = {
-//                    }) {
-//                        Icon(
-//                            imageVector = Icons.Filled.Menu,
-//                            contentDescription = "Localized description"
-//                        )
-//                    }
-//                },
-//                actions = {
-//                    IconButton(onClick = { /* doSomething() */ }) {
-//                        Icon(
-//                            imageVector = Icons.Filled.Search,
-//                            contentDescription = "Localized description"
-//                        )
-//                    }
-//                    IconButton(onClick = { expanded = true }) {
-//                        Icon(
-//                            imageVector = Icons.Filled.MoreVert,
-//                            contentDescription = "Localized description"
-//                        )
-//                    }
-//                    DropdownMenu(expanded = expanded, onDismissRequest = { expanded = false }) {
-//                        DropdownMenuItem(
-//                            text = { Text("Edit") },
-//                            onClick = { /* Handle edit! */ },
-//                            leadingIcon = {
-//                                Icon(
-//                                    Icons.Outlined.Edit,
-//                                    contentDescription = null
-//                                )
-//                            })
-//                        DropdownMenuItem(
-//                            text = { Text("Settings") },
-//                            onClick = { /* Handle settings! */ },
-//                            leadingIcon = {
-//                                Icon(
-//                                    Icons.Outlined.Settings,
-//                                    contentDescription = null
-//                                )
-//                            })
-//                        MenuDefaults.Divider()
-//                        DropdownMenuItem(
-//                            text = { Text("Send Feedback") },
-//                            onClick = { /* Handle send feedback! */ },
-//                            leadingIcon = {
-//                                Icon(
-//                                    Icons.Outlined.Email,
-//                                    contentDescription = null
-//                                )
-//                            },
-//                            trailingIcon = { Text("F11", textAlign = TextAlign.Center) })
-//                    }
-//
-//                },
-//
-//                )
-//        },
-//
-//        bottomBar = {
-//            NavigationBar() {
-//                val navBackStackEntry by navController.currentBackStackEntryAsState()
-//                val currentRoute = navBackStackEntry?.destination?.route
-//                items.forEach {
-//                    NavigationBarItem(selected = currentRoute == it.route,
-//                        label = {
-//                            Text(
-//                                text = it.label,
-////                                color = if (currentRoute == it.route) Color.DarkGray else Color.LightGray
-//                            )
-//                        },
-//                        icon = {
-//                            Icon(
-//                                imageVector = it.icons, contentDescription = null,
-////                                tint = if (currentRoute == it.route) Color.DarkGray else Color.LightGray
-//                            )
-//
-//                        },
-//
-//                        onClick = {
-//                            if (currentRoute != it.route) {
-//
-//                                navController.graph?.startDestinationRoute?.let {
-//                                    navController.popBackStack(it, true)
-//                                }
-//
-//                                navController.navigate(it.route) {
-//                                    launchSingleTop = true
-//
-//                                    scope.launch {
-//                                        snackbarHostState.showSnackbar(
-//                                            it.label,
-//                                            withDismissAction = true,
-//                                            actionLabel = "ACTION"
-//                                        )
-//                                    }
-//                                }
-//
-//                            }
-//                        })
-//                }
-//            }
-//        }
-//    )
-//    { innerPadding ->
-//        Box(modifier = Modifier.padding(innerPadding)) {
-//            NavigationController(
-//                navController = navController,
-//            )
-//        }
-//    }
-//}
 
-//@Preview(showBackground = true)
-//@Composable
-//fun DefaultPreview() {
-//    JetUITheme {
-//        com.aldikitta.jetui.Drawer()
-//    }
-//}
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    JetUITheme {
+        Drawer()
+    }
+}
