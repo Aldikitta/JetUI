ea0bcf4f196429a2a40dc4cfdc861608dcb2ef85
Aldikitta <andi.kitta77@gmail.com>
2022-06-15 13:48:58 +0800

    add drawer

--- app/src/main/java/com/aldikitta/jetui/MainActivity.kt
+++ app/src/main/java/com/aldikitta/jetui/MainActivity.kt
@@ -4,20 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material.icons.filled.MoreVert
-import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.*
 import androidx.compose.material.icons.outlined.Edit
 import androidx.compose.material.icons.outlined.Email
 import androidx.compose.material.icons.outlined.Settings
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -27,7 +22,6 @@ import com.aldikitta.jetui.screens.Accounts
 import com.aldikitta.jetui.screens.Home
 import com.aldikitta.jetui.screens.Notifications
 import com.aldikitta.jetui.screens.Settings
-import com.aldikitta.jetui.ui.composable.Drawer
 import com.aldikitta.jetui.ui.theme.JetUITheme
 import kotlinx.coroutines.launch
 
@@ -40,7 +34,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Navigation()
+                    Drawer()
                 }
             }
         }
@@ -66,154 +60,304 @@ fun NavigationController(navController: NavHostController, modifier: Modifier = 
         composable(NavigationItem.Account.route) {
             Accounts()
         }
-
     }
-
-
 }
-
-
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Navigation() {
-    val navController = rememberNavController()
-    val items = listOf(
-        NavigationItem.Home,
-        NavigationItem.Settings,
-        NavigationItem.Notifications,
-        NavigationItem.Account
-    )
-    val snackbarHostState = remember {
-        SnackbarHostState()
-    }
+fun Drawer() {
+    val drawerState = rememberDrawerState(DrawerValue.Closed)
     val scope = rememberCoroutineScope()
-    var expanded by remember { mutableStateOf(false) }
-    Scaffold(
-        snackbarHost = { SnackbarHost(snackbarHostState) },
-        topBar = {
-            SmallTopAppBar(
-                title = { Text(text = "Netflix") },
-
-                navigationIcon = {
-                    IconButton(onClick = {
-                        scope.launch {
-
+    val items = listOf(Icons.Default.Favorite, Icons.Default.Face, Icons.Default.Email)
+    val selectedItem = remember { mutableStateOf(items[0]) }
+    ModalNavigationDrawer(
+        drawerState = drawerState,
+        drawerContent = {
+            items.forEach { item ->
+                NavigationDrawerItem(
+                    icon = { Icon(item, contentDescription = null) },
+                    label = { Text(item.name) },
+                    selected = item == selectedItem.value,
+                    onClick = {
+                        scope.launch { drawerState.close() }
+                        selectedItem.value = item
+                    },
+                    modifier = Modifier.padding(NavigationDrawerItemDefaults.ItemPadding)
+                )
+            }
+        }
+    ) {
+        val navController = rememberNavController()
+        val items = listOf(
+            NavigationItem.Home,
+            NavigationItem.Settings,
+            NavigationItem.Notifications,
+            NavigationItem.Account
+        )
+        val snackbarHostState = remember {
+            SnackbarHostState()
+        }
+        val scope = rememberCoroutineScope()
+        var expanded by remember { mutableStateOf(false) }
+        Scaffold(
+            snackbarHost = { SnackbarHost(snackbarHostState) },
+            topBar = {
+                SmallTopAppBar(
+                    title = { Text(text = "Netflix") },
+                    navigationIcon = {
+                        IconButton(onClick = {
+//                            if (drawerState.isClosed) ">>> Swipe >>>" else "<<< Swipe <<<"
+                            scope.launch {
+                                drawerState.open()
+                            }
+                        }) {
+                            Icon(
+                                imageVector = Icons.Filled.Menu,
+                                contentDescription = "Localized description"
+                            )
                         }
-                    }) {
-                        Icon(
-                            imageVector = Icons.Filled.Menu,
-                            contentDescription = "Localized description"
-                        )
-                    }
+                    },
+                    actions = {
+                        IconButton(onClick = { /* doSomething() */ }) {
+                            Icon(
+                                imageVector = Icons.Filled.Search,
+                                contentDescription = "Localized description"
+                            )
+                        }
+                        IconButton(onClick = { expanded = true }) {
+                            Icon(
+                                imageVector = Icons.Filled.MoreVert,
+                                contentDescription = "Localized description"
+                            )
+                        }
+                        DropdownMenu(expanded = expanded, onDismissRequest = { expanded = false }) {
+                            DropdownMenuItem(
+                                text = { Text("Edit") },
+                                onClick = { /* Handle edit! */ },
+                                leadingIcon = {
+                                    Icon(
+                                        Icons.Outlined.Edit,
+                                        contentDescription = null
+                                    )
+                                })
+                            DropdownMenuItem(
+                                text = { Text("Settings") },
+                                onClick = { /* Handle settings! */ },
+                                leadingIcon = {
+                                    Icon(
+                                        Icons.Outlined.Settings,
+                                        contentDescription = null
+                                    )
+                                })
+                            MenuDefaults.Divider()
+                            DropdownMenuItem(
+                                text = { Text("Send Feedback") },
+                                onClick = { /* Handle send feedback! */ },
+                                leadingIcon = {
+                                    Icon(
+                                        Icons.Outlined.Email,
+                                        contentDescription = null
+                                    )
+                                },
+                                trailingIcon = { Text("F11", textAlign = TextAlign.Center) })
+                        }
 
-                },
-                actions = {
-                    IconButton(onClick = { /* doSomething() */ }) {
-                        Icon(
-                            imageVector = Icons.Filled.Search,
-                            contentDescription = "Localized description"
-                        )
-                    }
-                    IconButton(onClick = { expanded = true }) {
-                        Icon(
-                            imageVector = Icons.Filled.MoreVert,
-                            contentDescription = "Localized description"
-                        )
-                    }
-                    DropdownMenu(expanded = expanded, onDismissRequest = { expanded = false }) {
-                        DropdownMenuItem(
-                            text = { Text("Edit") },
-                            onClick = { /* Handle edit! */ },
-                            leadingIcon = {
-                                Icon(
-                                    Icons.Outlined.Edit,
-                                    contentDescription = null
-                                )
-                            })
-                        DropdownMenuItem(
-                            text = { Text("Settings") },
-                            onClick = { /* Handle settings! */ },
-                            leadingIcon = {
-                                Icon(
-                                    Icons.Outlined.Settings,
-                                    contentDescription = null
-                                )
-                            })
-                        MenuDefaults.Divider()
-                        DropdownMenuItem(
-                            text = { Text("Send Feedback") },
-                            onClick = { /* Handle send feedback! */ },
-                            leadingIcon = {
-                                Icon(
-                                    Icons.Outlined.Email,
-                                    contentDescription = null
+                    },
+
+                    )
+            },
+
+            bottomBar = {
+                NavigationBar {
+                    val navBackStackEntry by navController.currentBackStackEntryAsState()
+                    val currentRoute = navBackStackEntry?.destination?.route
+                    items.forEach { it ->
+                        NavigationBarItem(selected = currentRoute == it.route,
+                            label = {
+                                Text(
+                                    text = it.label,
+//                                color = if (currentRoute == it.route) Color.DarkGray else Color.LightGray
                                 )
                             },
-                            trailingIcon = { Text("F11", textAlign = TextAlign.Center) })
-                    }
-
-                },
-
-                )
-        },
-
-        bottomBar = {
-            NavigationBar() {
-                val navBackStackEntry by navController.currentBackStackEntryAsState()
-                val currentRoute = navBackStackEntry?.destination?.route
-                items.forEach {
-                    NavigationBarItem(selected = currentRoute == it.route,
-                        label = {
-                            Text(
-                                text = it.label,
-//                                color = if (currentRoute == it.route) Color.DarkGray else Color.LightGray
-                            )
-                        },
-                        icon = {
-                            Icon(
-                                imageVector = it.icons, contentDescription = null,
+                            icon = {
+                                Icon(
+                                    imageVector = it.icons, contentDescription = null,
 //                                tint = if (currentRoute == it.route) Color.DarkGray else Color.LightGray
-                            )
+                                )
 
-                        },
+                            },
 
-                        onClick = {
-                            if (currentRoute != it.route) {
+                            onClick = {
+                                if (currentRoute != it.route) {
 
-                                navController.graph?.startDestinationRoute?.let {
-                                    navController.popBackStack(it, true)
-                                }
-
-                                navController.navigate(it.route) {
-                                    launchSingleTop = true
-
-                                    scope.launch {
-                                        snackbarHostState.showSnackbar(
-                                            it.label,
-                                            withDismissAction = true,
-                                            actionLabel = "ACTION"
-                                        )
+                                    navController.graph.startDestinationRoute?.let {
+                                        navController.popBackStack(it, true)
                                     }
-                                }
 
-                            }
-                        })
+                                    navController.navigate(it.route) {
+                                        launchSingleTop = true
+
+                                        scope.launch {
+                                            snackbarHostState.showSnackbar(
+                                                it.label,
+                                                withDismissAction = true,
+                                                actionLabel = "ACTION"
+                                            )
+                                        }
+                                    }
+
+                                }
+                            })
+                    }
                 }
             }
-        })
-    { innerPadding ->
-        Box(modifier = Modifier.padding(innerPadding)) {
-            NavigationController(
-                navController = navController,
-            )
+        )
+        { innerPadding ->
+            Box(modifier = Modifier.padding(innerPadding)) {
+                NavigationController(
+                    navController = navController,
+                )
+            }
         }
     }
 }
+//
+//@OptIn(ExperimentalMaterial3Api::class)
+//@Composable
+//fun Navigation() {
+//    val navController = rememberNavController()
+//    val items = listOf(
+//        NavigationItem.Home,
+//        NavigationItem.Settings,
+//        NavigationItem.Notifications,
+//        NavigationItem.Account
+//    )
+//    val snackbarHostState = remember {
+//        SnackbarHostState()
+//    }
+//    val scope = rememberCoroutineScope()
+//    var expanded by remember { mutableStateOf(false) }
+//    Scaffold(
+//        snackbarHost = { SnackbarHost(snackbarHostState) },
+//        topBar = {
+//            SmallTopAppBar(
+//                title = { Text(text = "Netflix") },
+//                navigationIcon = {
+//                    IconButton(onClick = {
+//                    }) {
+//                        Icon(
+//                            imageVector = Icons.Filled.Menu,
+//                            contentDescription = "Localized description"
+//                        )
+//                    }
+//                },
+//                actions = {
+//                    IconButton(onClick = { /* doSomething() */ }) {
+//                        Icon(
+//                            imageVector = Icons.Filled.Search,
+//                            contentDescription = "Localized description"
+//                        )
+//                    }
+//                    IconButton(onClick = { expanded = true }) {
+//                        Icon(
+//                            imageVector = Icons.Filled.MoreVert,
+//                            contentDescription = "Localized description"
+//                        )
+//                    }
+//                    DropdownMenu(expanded = expanded, onDismissRequest = { expanded = false }) {
+//                        DropdownMenuItem(
+//                            text = { Text("Edit") },
+//                            onClick = { /* Handle edit! */ },
+//                            leadingIcon = {
+//                                Icon(
+//                                    Icons.Outlined.Edit,
+//                                    contentDescription = null
+//                                )
+//                            })
+//                        DropdownMenuItem(
+//                            text = { Text("Settings") },
+//                            onClick = { /* Handle settings! */ },
+//                            leadingIcon = {
+//                                Icon(
+//                                    Icons.Outlined.Settings,
+//                                    contentDescription = null
+//                                )
+//                            })
+//                        MenuDefaults.Divider()
+//                        DropdownMenuItem(
+//                            text = { Text("Send Feedback") },
+//                            onClick = { /* Handle send feedback! */ },
+//                            leadingIcon = {
+//                                Icon(
+//                                    Icons.Outlined.Email,
+//                                    contentDescription = null
+//                                )
+//                            },
+//                            trailingIcon = { Text("F11", textAlign = TextAlign.Center) })
+//                    }
+//
+//                },
+//
+//                )
+//        },
+//
+//        bottomBar = {
+//            NavigationBar() {
+//                val navBackStackEntry by navController.currentBackStackEntryAsState()
+//                val currentRoute = navBackStackEntry?.destination?.route
+//                items.forEach {
+//                    NavigationBarItem(selected = currentRoute == it.route,
+//                        label = {
+//                            Text(
+//                                text = it.label,
+////                                color = if (currentRoute == it.route) Color.DarkGray else Color.LightGray
+//                            )
+//                        },
+//                        icon = {
+//                            Icon(
+//                                imageVector = it.icons, contentDescription = null,
+////                                tint = if (currentRoute == it.route) Color.DarkGray else Color.LightGray
+//                            )
+//
+//                        },
+//
+//                        onClick = {
+//                            if (currentRoute != it.route) {
+//
+//                                navController.graph?.startDestinationRoute?.let {
+//                                    navController.popBackStack(it, true)
+//                                }
+//
+//                                navController.navigate(it.route) {
+//                                    launchSingleTop = true
+//
+//                                    scope.launch {
+//                                        snackbarHostState.showSnackbar(
+//                                            it.label,
+//                                            withDismissAction = true,
+//                                            actionLabel = "ACTION"
+//                                        )
+//                                    }
+//                                }
+//
+//                            }
+//                        })
+//                }
+//            }
+//        }
+//    )
+//    { innerPadding ->
+//        Box(modifier = Modifier.padding(innerPadding)) {
+//            NavigationController(
+//                navController = navController,
+//            )
+//        }
+//    }
+//}
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    JetUITheme {
-        Navigation()
-    }
-}
+//@Preview(showBackground = true)
+//@Composable
+//fun DefaultPreview() {
+//    JetUITheme {
+//        com.aldikitta.jetui.Drawer()
+//    }
+//}
